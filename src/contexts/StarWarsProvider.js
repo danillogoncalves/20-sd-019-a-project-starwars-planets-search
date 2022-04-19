33d5c45f3537b797ab9f7b70d3a64ecd73974d53
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-04-18 21:50:38 -0300

    :recycle: refactor: Colocando os useEffect nos components.

--- src/contexts/StarWarsProvider.js
+++ src/contexts/StarWarsProvider.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import starWarsContext from './starWarsContext';
 import fetchApi from '../services/fetchApi';
@@ -12,29 +12,27 @@ const StarWarsProvider = (props) => {
   const [filterByName, setFilterByName] = useState(INTITAL_STATE_FILTER_BY_NAME);
   const [filteredData, setFilteredData] = useState([]);
 
-  useEffect(() => {
-    const fetchApiData = async () => {
-      const result = await fetchApi();
-      setData(result);
-    };
-    fetchApiData();
-  }, []);
+  const getFetchApiData = async () => {
+    const result = await fetchApi();
+    setData(result);
+  };
 
-  useEffect(() => {
-    console.log('teste');
+  const getFilteredData = (() => {
     const newdata = data.filter(({ name }) => {
       const testNameData = name.toLocaleLowerCase();
       const testNameFilter = (filterByName.name).toLocaleLowerCase();
       return testNameData.includes(testNameFilter);
     });
     setFilteredData(newdata);
-  }, [data, filterByName]);
+  });
 
   const { Provider } = starWarsContext;
   const { children } = props;
 
   const value = {
     data,
+    getFetchApiData,
+    getFilteredData,
     filteredData,
     filterByName,
     setFilterByName,
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import FilterByName from '../components/FilterByName';
 import Table from '../components/Table';
+import starWarsContext from '../contexts/starWarsContext';
 
 function Home() {
+  const {
+    data,
+    filterByName,
+    getFetchApiData,
+    getFilteredData } = useContext(starWarsContext);
+  useEffect(() => {
+    getFetchApiData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    getFilteredData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data, filterByName]);
   return (
     <main>
       <FilterByName />
